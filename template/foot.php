c196707c774740705f06ebabef322596824bd4c1
Rafael Zamana <rafael@zamana.com.br>
2013-10-01 00:38:18 -0300

    O herotabs necessita o jQuery 1.9.0 ou higher, então colocando o jQuery 1.9.1 devolta junto com o jQuery Migrate

--- template/foot.php
+++ template/foot.php
@@ -1,2 +1,3 @@
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-<script src="assets/js/firstclass.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script src="http://code.jquery.com/jquery-migrate-1.2.1.js"></script>
+<script src="assets/js/firstclass.js"></script>
